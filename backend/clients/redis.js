7153aedcbe49dfacafca49bd68c60e4ea39e0166
Ozan Orkun <ozanorkun1@gmail.com>
2021-02-24 23:50:47 +0300

    Update redis.js
    redis credentials updated

--- backend/clients/redis.js
+++ backend/clients/redis.js
@@ -2,8 +2,8 @@ const redis = require("redis");
 
 const getClient = () => {
   return redis.createClient({
-    host: "ec2-54-75-41-243.eu-west-1.compute.amazonaws.com",
-    port: 15279,
+    host: "ec2-54-77-170-18.eu-west-1.compute.amazonaws.com",
+    port: 31929,
     password:
       "pce645556c0992b06d88179b144aa6bcd9a1b414c5be10ad0ebbeb1f5006552bb",
   });
